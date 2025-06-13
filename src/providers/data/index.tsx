cf2f0bd14f0b5f621a3a0199210a2d50aada0970
Blu <dagbale16@gmail.com>
2025-06-13 11:18:24 +0100

    prj-setup

---
+++ src/providers/data/index.tsx
@@ -0,0 +1,9 @@
+import { GraphQLClient } from "@refinedev/nestjs-query";
+
+export const API_URL = 'https://api.crm.refine.dev'
+
+export const client = new GraphQLClient(API_URL, {
+    fetch: (url: string, options: RequestInit) => {
+        
+    }
+})
