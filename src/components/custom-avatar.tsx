0ade99d35e33599cc793e3e863078b5bef228a7e
Blu <dagbale16@gmail.com>
2025-06-15 12:44:13 +0100

    utilities

--- src/components/custom-avatar.tsx
+++ src/components/custom-avatar.tsx
@@ -1,20 +1,21 @@
 import { Avatar as AntdAvatar, AvatarProps } from 'antd'
 
 type Props = AvatarProps & {
-    name: string;
+    name?: string;
 }
 
 const CustomAvatar = ({ name, style, ...rest}: Props) => {
   return (
    <AntdAvatar
-    alt={'Kenny Olo'}
+    alt={name}
     size="small"
     style={{ 
         backgroundColor: '#87d068 ',
         display: 'flex',
         alignItems: 'center',
         border: 'none',
-    }}
+        ...style
+    }}{...rest}
    >
     KO
    </AntdAvatar>
