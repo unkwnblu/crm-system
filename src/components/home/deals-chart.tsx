7e74e16f8471d6265998b807a32f835a3294789c
Blu <dagbale16@gmail.com>
2025-06-15 20:37:54 +0100

    deals-chart

--- src/components/home/deals-chart.tsx
+++ src/components/home/deals-chart.tsx
@@ -1,8 +1,57 @@
 import { DollarOutlined } from "@ant-design/icons"
 import { Card } from "antd"
+import React, { useMemo } from 'react';
 import { Text } from "../text"
+import { Area, AreaConfig } from '@ant-design/plots'
+import { useList } from "@refinedev/core"
+import { DASHBOARD_DEALS_CHART_QUERY } from "@/graphql/querires"
+import { mapDealsData } from "@/utilities/helpers"
+import { GetFieldsFromList } from "@refinedev/nestjs-query";
+import { DashboardDealsChartQuery } from "@/graphql/types";
 
 const DealsChart = () => {
+  const { data } = useList<GetFieldsFromList<DashboardDealsChartQuery>>
+  ({
+    resource: 'dealStages',
+    meta: {
+      gqlQuery: DASHBOARD_DEALS_CHART_QUERY
+    }
+  });
+
+  const dealData = React.useMemo(() => {
+    return mapDealsData(data?.data);
+  }, [data?.data])
+
+  const config: AreaConfig = {
+    data: dealData,
+    xField: 'timeText',
+    yField: 'value',
+    isStack: false,
+    seriesField: 'state',
+    animation: true,
+    startOnZero: false,
+    smooth: true,
+    legend: {
+      offsetY: -6
+    },
+    yAxis: {
+      tickCount: 4,
+      label: {
+        formatter: (v: string) => {
+          return `$${Number(v) /1000}k`
+        }
+      }
+    },
+    tooltip: {
+      formatter: (data) => {
+        return {
+            name:data.state,
+            value: `$${Number(data.value) / 1000}k)}`
+          
+        }
+      }
+    }
+  }
   return (
     <Card 
       style={{ height: '100%'}}
@@ -23,7 +72,7 @@ const DealsChart = () => {
         </div>
       }
     >
-
+      <Area {...config} height={325} />
 
     </Card>
   )
